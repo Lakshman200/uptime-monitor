5fc10fc8a48a7dcc04cc85347ccefb6bcbeab220
Lakshman Thippalli <lakshmanthippalli@Mac.lan>
2025-04-23 17:13:02 -0400

    Make port dynamic for Render deployment

--- app.py
+++ app.py
@@ -21,4 +21,7 @@ def home():
     return "UptimeMonitor API is running 👀"
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5050)
+    import os
+	port = int(os.environ.get("PORT", 5050))
+	app.run(debug=False, host="0.0.0.0", port=port)
+
